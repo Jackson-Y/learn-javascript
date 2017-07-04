393a9dbab3852215127a5c9e50533cb32735b96f
moxiao <827297635@qq.com>
2017-07-04 12:43:53 +0800

    modify lesson10-function.js
    Signed-off-by: moxiao <827297635@qq.com>

--- lesson10-function.js
+++ lesson10-function.js
@@ -109,3 +109,37 @@ function standStr(str) {
 }
 var str = ['asL', 'LNMP', 'aDLD', 'LISa', 'barT'];
 console.log(str.map(standStr));
+
+
+// 判断素数
+function isPrime(x) {
+    if (x === 1) {
+        return false;
+    }
+    for (var i = 2; i <= Math.sqrt(x); ++i) {
+        if (x % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function get_primes(arr) {
+    return arr.filter(isPrime);
+}
+
+var
+    x,
+    r,
+    arr = [];
+
+for (x = 1; x < 100; ++x) {
+    arr.push(x);
+}
+r = get_primes(arr);
+console.log(r);
+if (r.toString() === [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97].toString()) {
+    console.log('测试通过！');
+} else {
+    console.log('测试失败！');
+}
